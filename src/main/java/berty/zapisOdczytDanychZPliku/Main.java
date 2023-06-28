725a9a4872db2381f9855c38583645cc2045adbe
Bartossz Dolnik <bartosz.dolnik@gmail.com>
2023-06-28 17:52:00 +0200

    kolko i krzyzyk

--- src/main/java/berty/zapisOdczytDanychZPliku/Main.java
+++ src/main/java/berty/zapisOdczytDanychZPliku/Main.java
@@ -1,31 +1,11 @@
 package berty.zapisOdczytDanychZPliku;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj wymiar tabeli");
-        int rows = scanner.nextInt();
-        int columns = scanner.nextInt();
-
-        int[][] tabela = new int[rows][columns];
-
-        for (int i = 0; i < rows; i++) {
-
-            for (int j = 0; j < rows; j++) {
-                System.out.print("Wiersz " + (i + 1) + ", Kolumna " + (j + 1) + ": ");
-                tabela[i][j] = scanner.nextInt();
-            }
-        }
-
-        for(int i = 0; i <tabela.length; i++){
-            System.out.println(" ");
-            for(int j = 0; j < tabela[i].length; j++){
-                System.out.print(tabela[i][j]+ " ");
-
-
-            }
-        }
+    public static void main(String[] args) throws IOException  {
+        UserData userData = new UserData();
+userData.data();
     }
 }
---
+++ src/main/java/berty/zapisOdczytDanychZPliku/TestDataMain.java
@@ -0,0 +1,11 @@
+package berty.zapisOdczytDanychZPliku;
+
+import javax.imageio.IIOException;
+import java.io.IOException;
+
+public class TestDataMain {
+    public static void main(String[] args) throws IOException {
+        TestData testData = new TestData();
+        testData.data();
+    }
+}
